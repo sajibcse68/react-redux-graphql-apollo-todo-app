22eb541fbfa61b7fef81281cc4f7a000bd1d9be3
sajibcse68 <sajibcse68@gmail.com>
2019-08-19 15:27:05 +0600

    Develop basic todo app using React

---
+++ src/components/TodosList.js
@@ -0,0 +1,36 @@
+import React from "react";
+// import TodosListheader from "./TodosListHeader";
+import TodosListItem from "./TodosListItem";
+
+export default class TodosList extends React.Component {
+  renderItems() {
+    return this.props.todos.map((c, index) => {
+    console.log('todos: ', {...c})
+
+      return (
+        <TodosListItem
+          key={index}
+          {...c}
+          id={index}
+          toggleTask={this.props.toggleTask}
+          editTask={this.props.editTask}
+          deleteTask={this.props.deleteTask}
+        />
+      );
+    });
+  }
+  render() {
+    if(!this.props.todos.length) {
+      return <p className="tutorial">Create your first todo!</p>
+    }
+
+    return (
+      <table>
+         {/* <TodosListHeader /> */}
+        <tbody>
+          {this.renderItems()}
+        </tbody>
+      </table>
+    )
+  }
+}
