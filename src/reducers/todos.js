d1163184c4b5afc4e826477be759bd8759744058
sajibcse68 <sajibcse68@gmail.com>
2019-08-20 18:03:39 +0600

    Set redux for editTodo, deleteTodo. Setup localStorage for saving todos items

--- src/reducers/todos.js
+++ src/reducers/todos.js
@@ -1,33 +1,55 @@
+const setTodos = todos => {
+  localStorage.setItem("todos", JSON.stringify(todos));
+};
+
+const getTodos = () => {
+  return JSON.parse(localStorage.getItem("todos")) || [];
+};
+
 const todos = (state = [], action) => {
   switch (action.type) {
     case "ADD_TODO":
-      return [
-        ...state,
-        {
-          id: action.id,
-          task: action.text,
-          isCompleted: false
-        }
-      ]
-    case 'TOGGLE_TODO':
-      return state.map(todo => 
-        (todo.id === action.id)
-          ? {...todo, isCompleted: !todo.isCompleted}
+      setTodos([...getTodos(), {
+        id: state.length,
+        task: action.text,
+        isCompleted: false
+      }]);
+      console.log('....', getTodos());
+      return getTodos();
+    case "EDIT_TODO":
+      const todosAfterEdit = state.map(todo => {
+        const payload = action.payload;
+        return todo.id === payload.id ? { ...payload } : todo;
+      });
+      setTodos(todosAfterEdit);
+      return getTodos();
+    case "TOGGLE_TODO":
+      const todosAfterToggle = state.map(todo =>
+        todo.id === action.id
+          ? { ...todo, isCompleted: !todo.isCompleted }
           : todo
-      )
+      );
+      setTodos(todosAfterToggle);
+      return getTodos();
+    case "DELETE_TODO":
+      const todos = getTodos();
+      const newTodos = todos.filter(todo => todo.id !== action.id);
+      setTodos(newTodos);
+      return getTodos();
     default:
-      return [
-        {
-          id: 1,
-          task: "todo 1",
-          isCompleted: false
-        },
-        {
-          id: 2,
-          task: "todo 2",
-          isCompleted: false
-        }
-      ]
+      return getTodos();
+    // return [
+    //   {
+    //     id: 1,
+    //     task: "todo 1",
+    //     isCompleted: false
+    //   },
+    //   {
+    //     id: 2,
+    //     task: "todo 2",
+    //     isCompleted: false
+    //   }
+    // ]
   }
 };
 
